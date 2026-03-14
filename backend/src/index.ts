bd31b6032fdb8ba342f1db33e3dc187eb199c31c
Natnael Endale <n87294578@gmail.com>
2026-03-14 13:43:40 +0300

    add the connection to DB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,19 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+mongoose
+  .connect(
+    (process.env.LOCAL_DB as string) ||
+      "mongodb://localhost:27017/med-delivery",
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 const app = express();
 
 app.use(cors());
